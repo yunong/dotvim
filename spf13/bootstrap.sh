ca882285ac72d84cc8ac83fb47e9471676afda8e
yunong xiao <yjxiao@gmail.com>
2012-05-04 19:50:40 -0700

    added spf13

---
+++ spf13/bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/env sh
+
+endpath="$HOME/.spf13-vim-3"
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+echo "thanks for installing spf13-vim\n"
+
+# Backup existing .vim stuff
+echo "backing up current vim config\n"
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
+
+
+echo "cloning spf13-vim\n"
+git clone --recursive -b 3.0 http://github.com/spf13/spf13-vim.git $endpath
+mkdir -p $endpath/.vim/bundle
+ln -s $endpath/.vimrc $HOME/.vimrc
+ln -s $endpath/.vim $HOME/.vim
+
+echo "Installing Vundle"
+git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+
+echo "installing plugins using Vundle"
+vim +BundleInstall! +BundleClean +q
