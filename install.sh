b46fe6f2237c27cf54afb600b5c2534a1fa3afb0
Yunong Xiao <yunong@joyent.com>
2012-10-18 11:39:38 -0700

    add gcc-compiler to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pkgin up
-pkgin -y install gcc-runtime gmake scmgit python26 exctags
+pkgin -y install gcc-runtime gcc-compiler gmake scmgit python26 exctags
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
 ln -sf ~/dotvim/.vimrc ~/.
