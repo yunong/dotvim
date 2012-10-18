79f620f4cd8c2f60578cead7b451d7012e09d324
Yunong Xiao <yunong@joyent.com>
2012-10-18 11:22:13 -0700

    add gitconfig

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ pkgin up
 pkgin -y install gcc-runtime gmake scmgit python26 exctags
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
-ln -sf ~/dotvim/.vimrc
+ln -sf ~/dotvim/.vimrc ~/.
+ln -sf ~/dotvim/.gitconfig ~/.
