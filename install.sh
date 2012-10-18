34f726f520389119fd3efae66b3dbcb58cf17bc6
Yunong Xiao <yunong@joyent.com>
2012-10-18 11:32:56 -0700

    update README

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
 ln -sf ~/dotvim/.vimrc ~/.
 ln -sf ~/dotvim/.gitconfig ~/.
+scp yunong@10.99.99.254:~/.ssh/id_dsa ~/.ssh/id_dsa
