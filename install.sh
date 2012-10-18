bf7d689c4f0f393ef14a9288e18d0af5318c6dcc
Yunong Xiao <yunong@joyent.com>
2012-10-18 11:10:48 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pkgin -y install gcc-runtime gmake scmgit python26 exctags
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone git://github.com/yunong/dotvim.git ~/dotvim
+ln -sf ~/dotvim/.vimrc
+vim +BundleInstall +qall
