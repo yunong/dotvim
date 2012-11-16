d1f97bd84c018627da3100996d2d4dcd9e83f72e
Yunong Xiao <yunong@joyent.com>
2012-11-16 12:03:05 -0800

    force pkgin update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkgin up
+pkgin -f up
 pkgin -y install gcc-runtime gcc-compiler gmake scmgit python26 exctags
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
