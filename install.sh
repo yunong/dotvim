7ae9c1ac0f373ecd1075623e44fa7ded9fa6c0b3
Yunong Xiao <yunong@joyent.com>
2013-01-28 15:05:49 -0800

    add -y flag to pkgup

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkgin -f up
+pkgin -f -y up
 pkgin -y install gcc-runtime gcc-compiler gmake scmgit python26 exctags gsharutils
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
