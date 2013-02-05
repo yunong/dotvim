5de4a971a7f8e58bf938f2e6c0bf4bf256473042
Yunong Xiao <yunong@joyent.com>
2013-02-05 15:00:45 -0800

    add cmake and ruby to pkgsrc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pkgin -f -y up
-pkgin -y install gcc-runtime gcc-compiler gmake scmgit python26 exctags gsharutils gcc47 vim-7.3.712
+pkgin -y install gcc-runtime gcc-compiler gmake scmgit python26 exctags gsharutils gcc47 vim-7.3.712 cmake-2.8.10nb1 ruby193-base
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
 ln -sf ~/dotvim/.vimrc ~/.
