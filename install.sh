9c66fe9ad455154b13469181999d832b4dac3772
Yunong Xiao <yunong@joyent.com>
2013-02-15 15:36:19 -0800

    don't use ssl for vundle

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 pkgin -f -y up
 pkgin -y install gcc-runtime gcc-compiler gmake scmgit python26 exctags gsharutils gcc47 vim-7.3.712
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
 ln -sf ~/dotvim/.vimrc.smartos ~/.vimrc
 ln -sf ~/dotvim/.gitconfig ~/.
