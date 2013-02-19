2f09f83ca1def86200a55f1903fccdb7e8dca579
Yunong Xiao <yunong@joyent.com>
2013-02-19 11:30:02 -0800

    set git no ssl to true

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export GIT_SSL_NO_VERIFY=1
+echo 'export GIT_SSL_NO_VERIFY=1' >> ~/.bashrc
+source ~/.bashrc
 pkgin -f -y up
 pkgin -y install gcc-runtime gcc-compiler gmake scmgit python26 exctags gsharutils gcc47 vim-7.3.712
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
 ln -sf ~/dotvim/.vimrc.smartos ~/.vimrc
 ln -sf ~/dotvim/.gitconfig ~/.
