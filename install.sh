d2ef3a4638669c11080d731559908e99976304c7
Yunong Xiao <yunong@joyent.com>
2013-02-28 14:28:42 -0800

    fix here docs

--- install.sh
+++ install.sh
@@ -11,18 +11,20 @@ scp yunong@10.99.99.254:~/.ssh/id_dsa ~/.ssh/id_dsa
 
 # setup pbcopy
 cat > ~/pbcopy << HERE
-        cat > /tmp/pbcopy
-        scp /tmp/pbcopy yunong@10.99.99.254:/tmp/pbcopy > /dev/null
-        ssh -n yunong@10.99.99.254 "cat /tmp/pbcopy | pbcopy && rm /tmp/pbcopy" > /dev/null
-        rm /tmp/pbcopy
+#!/bin/bash
+cat > /tmp/pbcopy
+scp /tmp/pbcopy yunong@10.99.99.254:/tmp/pbcopy > /dev/null
+ssh -n yunong@10.99.99.254 "cat /tmp/pbcopy | pbcopy && rm /tmp/pbcopy" > /dev/null
+rm /tmp/pbcopy
 HERE
 chmod 755 ~/pbcopy
 
 cat > ~/pbpaste << HERE
-        ssh -n yunong@10.99.99.254 "pbpaste > /tmp/pbpaste" > /dev/null
-        scp yunong@10.99.99.254:/tmp/pbpaste /tmp/pbpaste > /dev/null
-        ssh -n yunong@10.99.99.254 "rm /tmp/pbpaste" > /dev/null
-        cat /tmp/pbpaste && rm /tmp/pbpaste
+#!/bin/bash
+ssh -n yunong@10.99.99.254 "pbpaste > /tmp/pbpaste" > /dev/null
+scp yunong@10.99.99.254:/tmp/pbpaste /tmp/pbpaste > /dev/null
+ssh -n yunong@10.99.99.254 "rm /tmp/pbpaste" > /dev/null
+cat /tmp/pbpaste && rm /tmp/pbpaste
 HERE
 chmod 755 ~/pbpaste
 
