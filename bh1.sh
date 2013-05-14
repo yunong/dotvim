450c98689318cd766e587288cb1a3cc836f45372
Yunong Xiao <yunong@joyent.com>
2013-05-14 11:34:41 -0700

    change bh1 ip

--- bh1.sh
+++ bh1.sh
@@ -7,23 +7,23 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone git://github.com/yunong/dotvim.git ~/dotvim
 ln -sf ~/dotvim/.vimrc.smartos ~/.vimrc
 ln -sf ~/dotvim/.gitconfig ~/.
-scp yunong@10.2.0.231:~/.ssh/id_dsa ~/.ssh/id_dsa
+scp yunong@10.2.0.234:~/.ssh/id_dsa ~/.ssh/id_dsa
 
 # setup pbcopy
 cat > ~/pbcopy << HERE
 #!/bin/bash
 cat > /tmp/pbcopy
-scp /tmp/pbcopy yunong@10.2.0.231:/tmp/pbcopy > /dev/null
-ssh -n yunong@10.2.0.231 "cat /tmp/pbcopy | pbcopy && rm /tmp/pbcopy" > /dev/null
+scp /tmp/pbcopy yunong@10.2.0.234:/tmp/pbcopy > /dev/null
+ssh -n yunong@10.2.0.234 "cat /tmp/pbcopy | pbcopy && rm /tmp/pbcopy" > /dev/null
 rm /tmp/pbcopy
 HERE
 chmod 755 ~/pbcopy
 
 cat > ~/pbpaste << HERE
 #!/bin/bash
-ssh -n yunong@10.2.0.231 "pbpaste > /tmp/pbpaste" > /dev/null
-scp yunong@10.2.0.231:/tmp/pbpaste /tmp/pbpaste > /dev/null
-ssh -n yunong@10.2.0.231 "rm /tmp/pbpaste" > /dev/null
+ssh -n yunong@10.2.0.234 "pbpaste > /tmp/pbpaste" > /dev/null
+scp yunong@10.2.0.234:/tmp/pbpaste /tmp/pbpaste > /dev/null
+ssh -n yunong@10.2.0.234 "rm /tmp/pbpaste" > /dev/null
 cat /tmp/pbpaste && rm /tmp/pbpaste
 HERE
 chmod 755 ~/pbpaste
