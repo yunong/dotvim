5b10150ae6f749ea29bd6b52cb8fa3518ba9cc0e
Yunong Xiao <yunong@users.noreply.github.com>
2015-08-16 22:22:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npm install -g eslint
 npm install -g jscs
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/yunong/dotvim.git ~/dotvim
 ln -sf ~/dotvim/.vimrc ~/.vimrc
 brew install ctags
